56cece3e14df5f4f3395435dae44be33e54ac982
Karol Stryczek <karol531531@gmail.com>
2021-04-09 20:03:21 +0200

    SUDOKU-40 Added plots.

--- playground.py
+++ playground.py
@@ -1,14 +1,13 @@
-from input.SudokuCSVReader import SudokuCSVReader, SudokuBoard
+from input.SudokuCSVReader import SudokuCSVReader
 from model.Mutator import *
 from model.SudokuSolver import SudokuSolver
-from utils.BoardGenerator import BoardGenerator
 
 do_mocking_start_pop = False
 do_read_from_mocking_start_pop = True
-chosen_sudoku_board = 1
-population_size = 2000
-cross_per_iter = 50
-max_iter = 1000
+chosen_sudoku_board = 0
+population_size = 1000
+cross_per_iter = 200
+max_iter = 50
 max_no_improve = 10
 mutator = SmartDuplicatesColumnMutator()
 
@@ -25,8 +24,7 @@ else:
     if do_read_from_mocking_start_pop:
         read_mock = True
         # reading mocked:
-        sudoku_num = 0
-        base = SudokuCSVReader(rf'./input/mocking_start_pops/mock_start_{sudoku_num}.pkl', read_mock).read_from_mock(population_size)
+        base = SudokuCSVReader(rf'./input/mocking_start_pops/mock_start_{chosen_sudoku_board}.pkl', read_mock).read_from_mock(population_size)
         solver = SudokuSolver(SudokuBoard(base[0]), population_size, cross_per_iter, mutator)
         board_list = list()
         for board_elem in range (1, len(base)):
